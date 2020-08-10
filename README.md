fe84f8a460affbbb486fe64c1c15a00ddc39008f
Kongduino <dbvic@me.com>
2020-08-10 14:14:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# Uno + RAK811 Shield (aka WisNode LoRa) Example
+
+## Introduction
+
+This sketch shows how to use a [WisNode LoRa](https://store.rakwireless.com/products/rak811-lpwan-evaluation-board) with an Arduino Uno or similar to send data from various environmental sensors as LoRa packets. In this case I'm using the following sensors:
+
+1. A [BME280 sensor](https://www.seeedstudio.com/Grove-BME280-Environmental-Sensor-Temperature-Humidity-Barometer.html), from SeeedStudio. I'm using [their library](https://github.com/Seeed-Studio/Grove_BME280).
+2. An [HM3301 Laser PM2.5 Dust Sensor](https://www.seeedstudio.com/Grove-Laser-PM2-5-Sensor-HM3301.html), again from Seeed. Using [their library](https://github.com/Seeed-Studio/Seeed_PM2_5_sensor_HM3301).
+3. An [SHT15 Temperature + Humidity sensor](https://item.taobao.com/item.htm?spm=a1z09.2.0.0.75e72e8d5tdtmv&id=611658565292&_u=a250qoit0372) from Taobao. Using the [SHT1x sensor library for ESPx](https://github.com/practicalarduino/SHT1x).
+4. A [Multichannel Gas Sensor](https://wiki.seeedstudio.com/Grove-Multichannel-Gas-Sensor-V2/) again from SeeedStudio. They provide [a library](https://github.com/Seeed-Studio/Seeed_Arduino_MultiGas).
+
+## Operation
+
+The WisNode LoRa's firmware is AT based, which means that while it can be convenient to use, there are some limitations. The main one being that strings have to be hex-encoded: you don't send `01234`, you send `3031323334`. Also, there's a limitation on the length of the string you can send. Since we're sending quite a bit of data, we will send two strings instead of one, making it easier on ourselves:
+
+1. BME280, HM3301 and SHT15.
+2. The Multichannel Gas Sensor (4 data points).
+
+In the Serial consile, we'll display the data, plus whatever we're sending, in a fex dump that makes it easier to read and debug. I wrote a quick `hexDump` routine that does the job nicely:
+
+```c
+void hexDump(uint8_t *buf, uint16_t len) {
+  String s = "|", t = "| |";
+  Serial.println(F("   +.0 .1 .2 .3 .4 .5 .6 .7 .8 .9 .A .B .C .D .E .F + +0123456789abcdef+"));
+  Serial.println(F("   +------------------------------------------------+ +----------------+"));
+  for (uint16_t i = 0; i < len; i += 16) {
+    for (uint8_t j = 0; j < 16; j++) {
+      if (i + j >= len) {
+        s = s + "   "; t = t + " ";
+      } else {
+        uint8_t c = buf[i + j];
+        if (c < 16) s = s + "0";
+        s = s + String(c, HEX) + " ";
+        if (c < 32 || c > 127) t = t + ".";
+        else t = t + (char)c;
+      }
+    }
+    Serial.print((uint8_t)(i / 16), HEX); Serial.print(". "); Serial.println(s + t + "|");
+    s = "|"; t = "| |";
+  }
+  Serial.println(F("   +------------------------------------------------+ +----------------+"));
+}
+```
+
+Similarly, the `str2hex` function encodes the string we want to send into an hex string:
+
+```c
+void str2hex(uint8_t len) {
+  uint8_t px = 0;
+  for (uint8_t i = 0; i < len; i++) {
+    uint8_t x = tmp[i];
+    uint8_t y = x >> 4;
+    myLoRaBuffer[px++] = alphabet[y];
+    myLoRaBuffer[px++] = alphabet[x & 0x0f];
+  }
+}
+```
+We put the encoded string directly into the `myLoRaBuffer` char array that will be used to send the string.
+
+```c
+void encodeSend() {
+  str2hex(strlen(tmp));
+  hexDump((char *)myLoRaBuffer, 128);
+  mySerial.print("at+txc=1,1000,");
+  mySerial.print((char *)myLoRaBuffer);
+  mySerial.print("\r\n");
+}
+```
+
+I am using here an older version of the firmware, which can be recognized from the send comma: `at+txc=1,1000,`: it tells the WisNode LoRa to send the string `1` time, every `1000` milliseconds. The old firmware allowed you to send repeatedly the same string at an interval, without having to do it every time. The new firmware removed that. The new command is `at+send=lorap2p:<data>`. In this case the reppetition is not needed and superfluous, but we could imagine a scenario where we needed that.
+
+We start the string with a UUID: we want to make sure the LoRa receiver(s) know whether this data stream is a repeat or a new one.
+
+```c
+char alphabet[37] = "0123456789abcdefghijklmnopqrstuvwxyz";
+// To hex-encode the message, and to produce a UUID
+
+[...]
+
+  memset(myLoRaBuffer, 0, 128);
+  uint8_t px = 0;
+  for (uint8_t i = 0; i < 8; i++) {
+    tmp[px + i] = alphabet[random(0, 35)];
+  }
+  px = strlen(tmp);
+```
+
+Note that in the setup, we do not initialize the WisNode LoRa: since it saves its settings, and reloads them at boot, we don't need to do that. However, using the old firmware I set it up with: `at+rf_config=433125000,SF,BW,CR,PREAMBLE,POWER`. In `loop()` we call repeatedly the two functions that produce the data and send it, and wait 5 seconds.
+
+
+
+
+
+
+
+
+
+
--- Uno_RAK811.ino
+++ Uno_RAK811.ino
@@ -17,7 +17,7 @@ uint8_t myLoRaBuffer[128];
 uint8_t tmp[80];
 
 char alphabet[37] = "0123456789abcdefghijklmnopqrstuvwxyz";
-// To hexencode the message, and to produce a UUID
+// To hex-encode the message, and to produce a UUID
 
 const char* str[] = {
   "Check code: %u",
@@ -60,23 +60,16 @@ void hexDump(uint8_t *buf, uint16_t len) {
   Serial.println(F("   +------------------------------------------------+ +----------------+"));
 }
 
-uint8_t px = 0;
-void str2hex(uint8_t ix, uint16_t value) {
-  uint8_t len;
-  memset(tmp, 0, 80);
-  if (ix == 0 || ix == 11) {
-    memset(myLoRaBuffer, 0, 128);
-    px = 0;
-    for (uint8_t i = 0; i < 8; i++) {
-      tmp[px + i] = alphabet[random(0, 35)];
-    }
-    sprintf(tmp + 8, ",%u", value);
-  } else if (ix < 7 || ix > 10) sprintf(tmp, ",%u", value);
-  else {
-    String v = "," + String(value / 100.0, 2);
-    v.toCharArray(tmp, v.length() + 1);
-  }
-  len = strlen(tmp);
+void encodeSend() {
+  str2hex(strlen(tmp));
+  hexDump((char *)myLoRaBuffer, 128);
+  mySerial.print("at+txc=1,1000,");
+  mySerial.print((char *)myLoRaBuffer);
+  mySerial.print("\r\n");
+}
+
+void str2hex(uint8_t len) {
+  uint8_t px = 0;
   for (uint8_t i = 0; i < len; i++) {
     uint8_t x = tmp[i];
     uint8_t y = x >> 4;
@@ -85,56 +78,37 @@ void str2hex(uint8_t ix, uint16_t value) {
   }
 }
 
-HM330XErrorCode print_result(uint8_t ix, uint16_t value) {
-  if (NULL == str) {
-    return ERROR_PARAM;
-  }
-  if (ix < 7 || ix > 10) sprintf(tmp, str[ix], value);
-  else {
-    float v = (float)value / 100.0;
-    String x = String((char*)str[ix]) + String(v, 2);
-    x.toCharArray(tmp, x.length() + 1);
-  }
-  Serial.println((char*)tmp);
-  str2hex(ix, value);
-  return NO_ERROR;
-}
-
-/*parse myBuffer with 29 uint8_t-data*/
 HM330XErrorCode parse_result(uint8_t* data) {
   uint16_t value = 0;
   uint32_t val = 0;
   if (NULL == data) {
     return ERROR_PARAM;
   }
+  memset(tmp, 0, 80);
+  memset(myLoRaBuffer, 0, 128);
+  uint8_t px = 0;
+  for (uint8_t i = 0; i < 8; i++) {
+    tmp[px + i] = alphabet[random(0, 35)];
+  }
+  px = strlen(tmp);
+  // Serial.println("px = " + String(px)); Serial.println((char*)tmp);
   for (int i = 1; i < 8; i++) {
     value = (uint16_t) data[i * 2] << 8 | data[i * 2 + 1];
-    print_result(i - 1, value);
+    sprintf(tmp + px, ",%u", value);
+    px = strlen(tmp);
+    // Serial.println("px = " + String(px)); Serial.println((char*)tmp);
   }
-  print_result(7, (uint16_t)bme280.getTemperature() * 100);
-  print_result(8, (uint16_t)bme280.getHumidity() * 100);
-  print_result(9, (uint16_t)sht15.readTemperatureC() * 100);
-  print_result(10, (uint16_t)sht15.readHumidity() * 100);
-
-  hexDump((char *)myLoRaBuffer, 128);
-  mySerial.print("at+txc=1,1000,");
-  mySerial.print((char *)myLoRaBuffer);
-  mySerial.print("\r\n");
-  delay(5000);
-
-  print_result(11, 1);
-  val = gas.getGM102B();
-  print_result(12, (uint16_t)val);
-  val = gas.getGM302B();
-  print_result(13, (uint16_t)val);
-  val = gas.getGM502B();
-  print_result(14, val);
-  val = gas.getGM702B();
-  print_result(15, (uint16_t)val);
-  hexDump((char *)myLoRaBuffer, 128);
-  mySerial.print("at+txc=1,1000,");
-  mySerial.print((char *)myLoRaBuffer);
-  mySerial.print("\r\n");
+  String myFloats = String((char*)tmp);
+  float fValue = bme280.getTemperature();
+  myFloats += "," + String(fValue, 2);
+  fValue = bme280.getHumidity();
+  myFloats += "," + String(fValue, 2);
+  fValue = sht15.readTemperatureC();
+  myFloats += "," + String(fValue, 2);
+  fValue = sht15.readHumidity();
+  myFloats += "," + String(fValue, 2);
+  myFloats.toCharArray(tmp, myFloats.length() + 1);
+  encodeSend();
   return NO_ERROR;
 }
 
@@ -149,8 +123,35 @@ HM330XErrorCode parse_result_value(uint8_t* data) {
   if (sum != data[28]) {
     Serial.println("wrong checkSum!!!!");
   }
-  Serial.println("");
+  //  Serial.println("Correct checksum");
   return NO_ERROR;
+}
+
+void doGasSensors() {
+  memset(tmp, 0, 80);
+  memset(myLoRaBuffer, 0, 128);
+  uint8_t px = 0;
+  for (uint8_t i = 0; i < 8; i++) {
+    tmp[px + i] = alphabet[random(0, 35)];
+  }
+  px = strlen(tmp);
+  uint32_t value = 1;
+  sprintf(tmp + px, ",%u", value);
+  px = strlen(tmp);
+  value = gas.getGM102B();
+  sprintf(tmp + px, ",%u", value);
+  px = strlen(tmp);
+  value = gas.getGM302B();
+  sprintf(tmp + px, ",%u", value);
+  px = strlen(tmp);
+  value = gas.getGM502B();
+  sprintf(tmp + px, ",%u", value);
+  px = strlen(tmp);
+  value = gas.getGM702B();
+  sprintf(tmp + px, ",%u", value);
+  px = strlen(tmp);
+  encodeSend();
+  delay(5000);
 }
 
 void setup() {
@@ -177,16 +178,18 @@ void setup() {
   delay(1000);
   while (mySerial.available()) Serial.write(mySerial.read());
   Serial.write('\n');
-  randomSeed(analogRead(0));
+  randomSeed(analogRead(3));
 }
 
 void loop() {
   if (sensor.read_sensor_value(myBuffer, 29)) {
     Serial.println("HM330X read result failed!!!");
   } else {
+    // Do the HM3301
     parse_result_value(myBuffer);
     parse_result(myBuffer);
     Serial.println("");
   }
-  delay(10000);
+  delay(5000);
+  doGasSensors();
 }
